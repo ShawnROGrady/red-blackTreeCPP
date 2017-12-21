0bb9c3615855b70a0147fd470455994be1e6eecd
Shawn O'Grady <33360408+ShawnROGrady@users.noreply.github.com>
2017-12-21 17:03:09 -0600

    declared checkInsert
    Declared protected function checkInsert() which will be performed after a node is inserted to the tree. This function will be used to maintain the balance of the tree following an insertion

--- red-BlackTree.cpp
+++ red-BlackTree.cpp
@@ -1,87 +1,30 @@
 //
-//  red-BlackTree.cpp
+//  red-BlackTree.hpp
 //  RedBlackTree
 //
 //  Created by Shawn O'Grady on 12/21/17.
 //  Copyright © 2017 Shawn O'Grady. All rights reserved.
 //
-/*
- For a red-black tree, every node follows the following rules (along w/ standard BST rules):
-    1. Every node has a color (red or black)
-    2. Root of the tree is always black
-    3. There ar no two adjacent red nodes
-        -i.e. a red node can't have a red parent or a red child
-    4. Every path from root to a NULL node has same number of black nodes
-*/
-#include <iostream>
-#include "red-BlackTree.hpp"
 
-redBlackTree::redBlackTree(){
-    root=NULL;
-    blackHeight=0;
-}
+#ifndef red_BlackTree_hpp
+#define red_BlackTree_hpp
 
-redBlackNode *redBlackTree::getRoot(){
-    return root;
-}
-
-void redBlackTree::insertNode(int newEntry){
-    redBlackNode *newNode=new redBlackNode(newEntry);
-    redBlackNode *tmp=root;
-    redBlackNode *parent=nullptr;
-    while(tmp!=NULL){
-        parent=tmp;
-        if(newNode->getValue()<tmp->getValue()){
-            tmp=tmp->getLeftChild();
-        }else{
-            tmp=tmp->getRightChild();
-        }
-    }
-    if(parent==NULL){
-        //tree is empty
-        newNode->setBlack(true);    //root is black (rule 2)
-        blackHeight++;
-        root=newNode;
-    }
-    else if (newNode->getValue()<parent->getValue()){
-        parent->setLeftChild(newNode);
-        newNode->setParent(parent);
-    }
-    else{
-        parent->setRightChild(newNode);
-        newNode->setParent(parent);
-    }
-}
-void redBlackTree::traverseAndPrintInorder(redBlackNode *root){
-    if(root==NULL){
-        return;
-    }
-    else{
-        traverseAndPrintInorder(root->getLeftChild());
-        std::cout<<root->getValue();
-        std::cout<<std::endl;
-        traverseAndPrintInorder(root->getRightChild());
-    }
-}
-void redBlackTree::traverseAndPrintPreorder(redBlackNode *root){
-    if(root==NULL){
-        return;
-    }
-    else{
-        std::cout<<root->getValue();
-        std::cout<<std::endl;
-        traverseAndPrintPreorder(root->getLeftChild());
-        traverseAndPrintPreorder(root->getRightChild());
-    }
-}
-void redBlackTree::traverseAndPrintPostorder(redBlackNode *root){
-    if(root==NULL){
-        return;
-    }
-    else{
-        traverseAndPrintPostorder(root->getLeftChild());
-        traverseAndPrintPostorder(root->getRightChild());
-        std::cout<<root->getValue();
-        std::cout<<std::endl;
-    }
-}
+#include <stdio.h>
+#include "red-blackNode.hpp"
+class redBlackTree{
+private:
+    redBlackNode *root;
+    int blackHeight;
+protected:
+    void checkInsert(redBlackNode* newNode);
+public:
+    redBlackTree();
+    bool isEmpty();
+    redBlackNode *getRoot();
+    void insertNode(int newEntry);
+    void removeNode(redBlackNode* target);
+    void traverseAndPrintInorder(redBlackNode *root);
+    void traverseAndPrintPreorder(redBlackNode *root);
+    void traverseAndPrintPostorder(redBlackNode *root);
+};
+#endif /* red_BlackTree_hpp */
