26612666851b5586de919416f094bc3fa96f329c
Shawn O'Grady <33360408+ShawnROGrady@users.noreply.github.com>
2017-12-21 16:17:07 -0600

    Add files via upload
    Files include basic outline+partial implementation of red black tree. Currently, only have prints fully implemented and insert function behaves like a standard BST insertion

---
+++ red-BlackTree.hpp
@@ -0,0 +1,28 @@
+//
+//  red-BlackTree.hpp
+//  RedBlackTree
+//
+//  Created by Shawn O'Grady on 12/21/17.
+//  Copyright © 2017 Shawn O'Grady. All rights reserved.
+//
+
+#ifndef red_BlackTree_hpp
+#define red_BlackTree_hpp
+
+#include <stdio.h>
+#include "red-blackNode.hpp"
+class redBlackTree{
+private:
+    redBlackNode *root;
+    int blackHeight;
+public:
+    redBlackTree();
+    bool isEmpty();
+    redBlackNode *getRoot();
+    void insertNode(int newEntry);
+    void removeNode(redBlackNode* target);
+    void traverseAndPrintInorder(redBlackNode *root);
+    void traverseAndPrintPreorder(redBlackNode *root);
+    void traverseAndPrintPostorder(redBlackNode *root);
+};
+#endif /* red_BlackTree_hpp */
