4a4fbfbeeae899a2cdbca3a8984a478706ce5273
Shawn O'Grady <33360408+ShawnROGrady@users.noreply.github.com>
2017-12-22 14:38:44 -0600

    Added new tests
    I have added new tests to verify that the insert function behaves as it should. I was a bit confused since I assumed the tree would be perfectly balanced when it contains 1-7, but after drawing the state of the tree I now understand that even when not perfectly balanced it still follows the rules of the red-black tree.

--- main.cpp
+++ main.cpp
@@ -18,6 +18,21 @@
 
 int main(int argc, const char * argv[]) {
     redBlackTree *tree=new redBlackTree();
+    /*
+    for(int i=1; i<8; i++){
+        tree->insertNode(i);
+    }
+    //tree->traverseAndPrintInorder(tree->getRoot()); // 1 2 3 4 5 6 7
+    //tree->traverseAndPrintPreorder(tree->getRoot()); // 2 1 4 3 6 5 7
+    tree->traverseAndPrintPostorder(tree->getRoot()); // 1 3 5 7 6 4 2
+    */
+    for(int i=1; i<9; i++){
+        tree->insertNode(i);
+    }
+    //tree->traverseAndPrintInorder(tree->getRoot()); // 1 2 3 4 5 6 7 8
+    //tree->traverseAndPrintPreorder(tree->getRoot()); // 4 2 1 3 6 5 7 8
+    tree->traverseAndPrintPostorder(tree->getRoot()); // 1 3 2 5 8 7 6 4
+    /*
     tree->insertNode(10);
     tree->insertNode(20);
     tree->insertNode(30);
@@ -26,5 +41,6 @@ int main(int argc, const char * argv[]) {
     //tree->traverseAndPrintInorder(tree->getRoot()); // 10 15 20 30
     tree->traverseAndPrintPreorder(tree->getRoot()); // 20 10 15 30
     //tree->traverseAndPrintPostorder(tree->getRoot()); // 30 15 10 20
+     */
     return 0;
 }
