9842ea9203af3e8a31de54ec2b126ee77fb45cc3
Shawn O'Grady <33360408+ShawnROGrady@users.noreply.github.com>
2018-01-05 16:12:37 -0600

    declared doubleBlack getters+setters
    Declared a new property of each node called doubleBlack, along with the necessary setters+getters. This property is required for maintaining the balance of a red black tree when removing a node.

--- red-blackNode.hpp
+++ red-blackNode.hpp
@@ -14,6 +14,7 @@ class redBlackNode{
 private:
     int value;
     bool black;
+    bool doubleBlack;
     redBlackNode *parent;
     redBlackNode *leftChild;
     redBlackNode *rightChild;
@@ -24,12 +25,14 @@ public:
     //setters:
     void setValue(int v);
     void setBlack(bool b);
+    void setDoubleBlack(bool b);
     void setParent(redBlackNode *newParent);
     void setLeftChild(redBlackNode *newLeftChild);
     void setRightChild(redBlackNode *newRightChild);
     //getters
     int getValue();
     bool getBlack();
+    bool getDoubleBlack();
     redBlackNode* getParent();
     redBlackNode* getLeftChild();
     redBlackNode* getRightChild();
