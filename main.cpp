f65440d98121b8f3703c0322769efc54851c1a17
Shawn O'Grady <33360408+ShawnROGrady@users.noreply.github.com>
2018-01-19 16:04:56 -0600

    Added insert test cases
    Added tests to verify that tree behaves properly for each insertion case.

--- main.cpp
+++ main.cpp
@@ -17,6 +17,65 @@
 //test function:
 
 int main(int argc, const char * argv[]) {
+    redBlackTree *tree=new redBlackTree();
+    /*
+     Cases to test for when inserting a node:
+        1. new node is root
+        2. new node's parent is red
+            a) new nodes uncle is red
+            b) new node's uncle is black
+                i) new node is left child to parent and parent is left child to grandparent (left-left)
+                ii) new node is right child to parent and parent is left child to grandparent (left-right)
+                iii) new node is right child to parent and parent is right child to grandparent (right-right)
+                iv) new node is left child to parent and parent is right child to grandparent (right-left)
+     */
+    /*
+    //Case 1:
+    tree->insertNode(2);
+    tree->traverseAndPrintPreorder(tree->getRoot()); //2 black
+     */
+    /*
+    //Case 2.a:
+    tree->insertNode(2);
+    tree->insertNode(1);
+    tree->insertNode(3);
+    //tree->traverseAndPrintPreorder(tree->getRoot()); //2 black, 1 red, 3 red
+    tree->insertNode(4);
+    tree->traverseAndPrintPreorder(tree->getRoot()); //2 black, 1 black, 3 black, 4 red
+     */
+    /*
+    //Case 2.b.i:
+    for(int i=4; i>0; i--){
+        tree->insertNode(i);
+    }
+    //tree->traverseAndPrintPreorder(tree->getRoot());    //3 black, 2 black, 1 red, 4 black
+    tree->insertNode(0);
+    tree->traverseAndPrintPreorder(tree->getRoot());    //3 black, 1 black, 0 red, 2 red, 4 black
+     */
+    /*
+    //Case 2.b.ii:
+    tree->insertNode(6);
+    tree->insertNode(4);
+    //tree->traverseAndPrintPreorder(tree->getRoot()); //6 black, 4 red
+    tree->insertNode(5);
+    tree->traverseAndPrintPreorder(tree->getRoot()); //5 black, 4 red, 6 red
+     */
+    /*
+    //Case 2.b.iii:
+    tree->insertNode(1);
+    tree->insertNode(2);
+    tree->insertNode(3);
+    tree->insertNode(4);
+    //tree->traverseAndPrintPreorder(tree->getRoot());    // 2 black, 1 black, 3 black, 4 red
+    tree->insertNode(5);
+    tree->traverseAndPrintPreorder(tree->getRoot()); //2 black, 1 black, 4 black, 3 red, 5 red
+     */
+    //Case 2.b.iv:
+    tree->insertNode(4);
+    tree->insertNode(6);
+    //tree->traverseAndPrintPreorder(tree->getRoot()); //4 black, 6 red
+    tree->insertNode(5);
+    tree->traverseAndPrintPreorder(tree->getRoot()); //5 black, 4 red, 6 red
     /*
      Cases to test for when removing node:
         1. replacement or target is red
@@ -32,7 +91,6 @@ int main(int argc, const char * argv[]) {
                 ii) sibling is right child to parent
      Got help coming up with test cases from: https://www.geeksforgeeks.org/red-black-tree-set-3-delete-2/
      */
-    redBlackTree *tree=new redBlackTree();
     /*
     //Case 1:
     tree->insertNode(30);
@@ -103,7 +161,7 @@ int main(int argc, const char * argv[]) {
     tree->removeNode(6);
     tree->traverseAndPrintPreorder(tree->getRoot()); //7 black, 3 black, 2 black, 1 red, 5 black, 4 red, 9 black, 8 black, 10 black
      */
-    
+    /*
     //Case 2.c.ii:
     for(int i=1; i<11; i++){
         tree->insertNode(i);
@@ -111,6 +169,6 @@ int main(int argc, const char * argv[]) {
     //tree->traverseAndPrintPreorder(tree->getRoot()); //4 black, 2 black, 1 black, 3 black, 6 black, 5 black, 8 red, 7 black, 9 black, 10 red
     tree->removeNode(5);
     tree->traverseAndPrintPreorder(tree->getRoot()); //4 black, 2 black, 1 black, 3 black, 8 black, 6 black, 7 red, 9 black, 10 red
-     
+     */
     return 0;
 }
